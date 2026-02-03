6b9ce96e67385e223867e13f11de4c47fddb1756
Yash8576 <yaswanth.dammalapati2002@gmail.com>
2026-02-02 19:03:16 -0500

    chore: remove webapp directory from git tracking

--- webapp/start-backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting Buzz Social Cart Backend...
-cd /d %~dp0backend
-C:\Users\dravi\Downloads\webapp\.venv\Scripts\python.exe -m uvicorn server:app --reload --port 8000
--- webapp/start-frontend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting Buzz Social Cart Frontend...
-cd /d %~dp0frontend
-npm run dev
